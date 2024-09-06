d99f043acbf4ced4e32974691928aa6ee546bb31
cuonggpham <qcuonghls@gmail.com>
2024-09-07 00:50:04 +0700

    create User schema

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express'
+import User from '~/models/schemas/User.schema'
+import usersService from '~/services/users.services'
 
 export const loginController = (req: Request, res: Response) => {
   const { email, password } = req.body
@@ -11,4 +13,19 @@ export const loginController = (req: Request, res: Response) => {
   return res.status(400).json({
     error: 'Login failed'
   })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const result = await usersService.register({ email, password })
+
+    return res.json({
+      message: 'Register success'
+    })
+  } catch (error) {
+    return res.status(400).json({
+      error: 'Register failed'
+    })
+  }
 }
