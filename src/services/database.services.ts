52c5ab2939d7722871341b5c83b7b8a3d18c0348
cuonggpham <qcuonghls@gmail.com>
2024-09-06 16:30:24 +0700

    connect MongoDB

---
+++ src/services/database.services.ts
@@ -0,0 +1,26 @@
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { config } from 'dotenv'
+config()
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.1n8mc.mongodb.net/?retryWrites=true&w=majority&appName=Twitter`
+
+class DatabaseService {
+  private client: MongoClient
+  constructor() {
+    this.client = new MongoClient(uri)
+  }
+
+  async connect() {
+    try {
+      // Send a ping to confirm a successful connection
+      await this.client.db('admin').command({ ping: 1 })
+      console.log('Pinged your deployment. You successfully connected to MongoDB!')
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await this.client.close()
+    }
+  }
+}
+
+// Create object from class DatabaseService
+const databaseService = new DatabaseService()
+export default databaseService
